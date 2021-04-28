b238befc9b0c0e3fd9fad004e48666f3dde2dfdd
nayeemdev <nayeem9812@gmail.com>
2021-04-28 22:28:42 +0600

    feat: Header component added with Add Task Button

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+    <button @click="addTask()" class="btn" :style="{background: bg_color}">
+        {{ name }}
+    </button>
+</template>
+
+<script>
+    export default {
+        name: 'Button',
+        props: {
+            name: String,
+            bg_color: String
+        },
+        methods: {
+            addTask() {
+                console.log('Task Added')
+            }
+        }
+    }
+</script>
